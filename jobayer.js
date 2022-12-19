438ab86672eda41eafa15a2b39af1d9f208f1c33
JubayerRahman <jubayerr398@gmail.com>
2022-12-19 14:05:35 +0600

    I made a adice gnerator with js this is a forntend mentor chanllenge

---
+++ jobayer.js
@@ -0,0 +1,17 @@
+//bg animation of jobayer
+
+window.addEventListener("DOMContentLoaded",()=>{
+    // VANTA.BIRDS({
+    VANTA.NET({
+        el: "#main",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x#52ffa8,
+        backgroundColor: 0x#1f2632
+            })
+    })
---
+++ sript.js
@@ -0,0 +1,14 @@
+const adviceSection = document.getElementById('adviceSection')
+const advice = document.getElementById('advice')
+const adviceId = document.getElementById('adviceId')
+const button = document.getElementById('button');
+
+button.addEventListener('click', async ()=>{
+    adviceSection.classList.remove('animation');
+    const url ="https://api.adviceslip.com/advice";
+    const data = await fetch (url);
+    const res = await data.json();
+    adviceId.innerHTML = res.slip.id;
+    advice.innerHTML = res.slip.advice;
+    adviceSection.classList.add('animation');
+})
